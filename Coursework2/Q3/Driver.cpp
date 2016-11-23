d5a7fdef6ca46f88855c15bdbf58ab1683f4a791
Jordi-Ab <jordi_a3@hotmail.com>
2016-11-23 08:17:18 +0000

    Just need to plot on Q3

--- Coursework2/Q3/Driver.cpp
+++ Coursework2/Q3/Driver.cpp
@@ -16,35 +16,43 @@ double infinityNorm(double* a_vector, int size);
 int main(){
 	int* ns = _ns();
 	for (int i=0; i<20; i++){
-		int n = ns[i];
+		int n = ns[i]+1;
 
 		//int n=10;
 
-		double* mesh = new double[n+1];
-		double** D = allocateMatrixMemory(n+1,n+1);
+		double* mesh = new double[n];
+		double** D = allocateMatrixMemory(n,n);
 
-		ChebyshevDifferentiationMatrix(n, mesh, D);
+		ChebyshevDifferentiationMatrix(n-1, mesh, D);
 		//PrintVector(n+1, mesh, "mesh");
 		//PrintMatrix(n+1,n+1, D, "Diff");
 
 		
-		double* us = f(mesh, n+1);
+		double* us = f(mesh, n);
 		//PrintVector(n+1, us, "us");
 
 		// D is symmetric, then you can just make half of the step for multiplication.
-		double* approx_sol = matrixTimesVector(D, us, n+1,n+1,n+1);
+		double* approx_sol = matrixTimesVector(D, us, n,n,n);
 		//PrintVector(n+1, approx_sol, "approx_sol");
-		double* true_deriv = df(mesh, n+1);
+		double* true_deriv = df(mesh, n);
 		//PrintVector(n+1, true_deriv, "true_sol");
-		double* residual = substractV(true_deriv, approx_sol, n+1);
+		double* residual = substractV(true_deriv, approx_sol, n);
 		//PrintVector(n+1, residual, "residual");
-		double error = infinityNorm(residual, n+1);
+		double error = infinityNorm(residual, n);
 
-		std::cout << "For n = " << n << ", ";
+		std::cout << "For n = " << n-1 << ", ";
 		std::cout << "error = " << error << std::endl;
 
+		if(n-1 == 40){
+		// Save vectors:
+			std::cout << "Saving vectors to files" << std::endl;
+			SaveVectorToFile(n, mesh, "grid.dat");
+			SaveVectorToFile(n, approx_sol, "du_numerical.dat");
+			SaveVectorToFile(n, true_deriv, "du_analytical.dat");
+		}
+
 		delete[] mesh;
-		freeMatrixMemory(n+1, D);
+		freeMatrixMemory(n, D);
 		delete[] us;
 		delete[] approx_sol;
 	}
